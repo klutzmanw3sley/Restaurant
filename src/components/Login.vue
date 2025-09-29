c1a31129701988d709f46604d0ef4ad4cc0b3364
unknown <abayohuncho@gmail.com>
2025-09-29 12:00:07 +0300

    user management

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,63 @@
-<script setup> 
+<script setup>
+import {ref} from 'vue'
+import {useRouter} from "vue-router"
 
-</script >
+const showPassword = ref(false)
+
+
+//models
+const email = ref(null)
+const password = ref(null)
+
+function login(){
+    try {
+        //get user data
+        let user = JSON.parse(localStorage.getItem( "signUpData"));
+        //check user details
+        if(email.value = user.email && password.value == user.password){
+            localStorage.setItem( "isLoggedIn", true );
+            router.push('/')
+
+        }else{
+            console.log("Invalid credentials")
+        }
+
+        // To Do: send data to backend
+    } catch (err) {
+        console.error('Sign up process failed', err)
+    }
+
+}
+</script>
 
 <template>
-    <h1>LogIn</h1>
-    
+    <v-container align="center">
+        <v-row>
+            <v-col>
+                <v-card class="pa-6" width="600" color="deep-purple-lighten-1">
+                    <v-card-title>Login</v-card-title>
+                    <v-text-field v-model="email" label="Email Address" :rules="[
+                            (v) => !!v || 'Email is required',
+                            (v) => /.+@.+\..+/.test(v) || 'Email must be valid',
+                        ]"
+                        required></v-text-field>
+                    <v-text-field v-model="password" label="Password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="showPassword ? 'text' : 'password'"
+                        @click:append="showPassword = !showPassword"
+                        :rules="[
+                            (v) => !!v || 'Password is required',
+                            (v) => v.length >= 8 || 'Password must be at least 8 characters',
+                        ]" ></v-text-field>
+                    
+                    <v-card-text>
+                        Don't have an  account?
+                        <router-link to="/signup"> Sign Up</router-link>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="white" variant="elevated" @click="Login()">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,81 @@
 <script setup> 
+import {ref} from 'vue'
+
+const showPassword =ref(false)
+const showConfirmPassword = ref(false)
+
+// models
+const name =ref(null)
+const email =ref(null)
+const phone =ref(null)
+const deliveryAddress =ref(null)
+const password = ref(null)
+
+
+function signup(){
+const signUpData = {
+    'name': name.value,
+     'email': email.value,
+      'phone': phone.value,
+       'deliveryAddress': deliveryAddress.value,
+        'password': password.value
+}
+ try{
+    // save data on browser
+    localStorage.setItem("SignUpData",JSON.stringify(signUpData));
+    // to Do: send data to backend
+
+} catch (err) {
+    console.error('Sign Up process failed', err)}
+
+  }
+
+
+
+
 
 </script >
 
 <template>
-    <h1>SignUp</h1>
+<v-container align="center">
+    <v-row>
+        <v-col>
+            <v-card class="pa-6" width="600" color="indigo-darken-4"> 
+                <v-card-title> Sign up </v-card-title>
+                <v-text-field  v-model="name" label="Name" :rules="[(v) => !!v || 'Name is required']"></v-text-field>
+                <v-text-field v-model="email" label="Email" 
+                                        :rules="[
+                            (v) => !!v || 'Email is required',
+                            (v) => /.+@.+\..+/.test(v) || 'Email must be valid',
+                        ]"></v-text-field>
+                <v-text-field v-model="phone" label="Phone Number" type="number"></v-text-field>
+                <v-text-field  v-model="deliveryAddress"label="Delivery Address" :rules="[(v) => !!v || 'Delivery Address is required']"></v-text-field>
+                <v-text-field v-model="password" label="Password"  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="showPassword ? 'text' : 'password'"
+                        @click:append="showPassword = !showPassword"
+                        :rules="[
+                            (v) => !!v || 'Password is required',
+                            (v) => v.length >= 8 || 'Password must be at least 8 characters',
+                        ]" ></v-text-field>
+                <v-text-field label="Confirm Password" :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="showConfirmPassword ? 'text' : 'password'"
+                        @click:append="showConfirmPassword = !showConfirmPassword"
+                        :rules="[
+                            (v) => !!v || 'Please confirm your password',
+                            (v) => v === password || 'Passwords must match',
+                        ]"></v-text-field>
+                <v-card-text>Already registerd? 
+                    <router-link to="/login">Back to login</router-link>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="white" variant="elevated" @click="signup()"> Sign Up</v-btn>
+                </v-card-actions>
+
+
+            </v-card>
+        </v-col>
+    </v-row>
+</v-container>
+   
     
 </template>
